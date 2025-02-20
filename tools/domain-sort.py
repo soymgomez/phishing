92e021fad4968ce3c194ec5cbe0c0709050f0ccb
Spirillen <44526987+spirillen@users.noreply.github.com>
2025-02-20 17:57:45 +0100

    breakthroughtechnologydialogue.com (whitelist)
    Fix https://github.com/Phishing-Database/Phishing.Database/issues/1150
    
    Also altered the domain-sort.py to remove duplicated lines

--- tools/domain-sort.py
+++ tools/domain-sort.py
@@ -2,5 +2,10 @@
 
 from fileinput import input
 
-for y in sorted([x.strip().split('.')[::-1] for x in input()]): print(
-    '.'.join(y[::-1]))
+seen = set()  # Set to track seen lines
+
+for y in sorted([x.strip().split('.')[::-1] for x in input()]):
+    line = '.'.join(y[::-1])
+    if line not in seen:  # Check if the line has already been seen
+        print(line)  # Print the line if it's not a duplicate
+        seen.add(line)  # Add the line to the set of seen lines
